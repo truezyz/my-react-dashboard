8056bd152908d22079634b49b96e456c92c0f2f5
truezyz <89248186+truezyz@users.noreply.github.com>
2025-12-16 07:41:09 +0000

    revise yml files

--- moving-average-demo/vite.config.ts
+++ moving-average-demo/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/truezyz.github.io/'
+  base: '/moving-average-demo/'
 })
